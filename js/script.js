b3c331815ad7022927d758264b8421e6fabb6caa
abrakib942 <abrakib942@gmail.com>
2022-02-17 18:09:19 +0600

    error handle input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 document.getElementById('calc-btn').addEventListener('click', function() {
+
+    getBalance()
+    
+})
+
+function getBalance() {
     let incomeInput = document.getElementById('income-input').value;
     let foodInput = document.getElementById('food-input').value;
     let rentInput = document.getElementById('rent-input').value;
@@ -8,11 +14,50 @@ document.getElementById('calc-btn').addEventListener('click', function() {
     let totalExpenses = document.getElementById('total-expenses');
     let totalBalance = document.getElementById('total-balance');
 
-    totalExpenses.innerText = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothsInput);
+    totalExpenses.innerText = Number(foodInput) + Number(rentInput) + Number(clothsInput);
+    let totalExp = totalExpenses.innerText;
 
-    totalBalance.innerText = parseFloat(incomeInput) - parseFloat(totalExpenses.innerText);
-    
-})
+    totalBalance.innerText = Number(incomeInput) - Number(totalExp);
+
+    let incomeError = document.getElementById('income-error');
+    let foodError = document.getElementById('food-error');
+    let rentError = document.getElementById('rent-error');
+    let clothsError = document.getElementById('cloths-error');
+    let balanceError = document.getElementById('balance-error');
+
+
+    /* error handle input */
+    if(isNaN(incomeInput) || incomeInput < 0) {
+        incomeError.style.display = 'block';
+
+        totalExpenses.innerText = 0;
+        totalBalance.innerText = 0;
+    }
+    if(isNaN(foodInput) || foodInput < 0) {
+        foodError.style.display = 'block';
+
+        totalExpenses.innerText = 0;
+        totalBalance.innerText = 0;
+    }
+    if(isNaN(rentInput) || rentInput < 0) {
+        rentError.style.display = 'block';
+
+        totalExpenses.innerText = 0;
+        totalBalance.innerText = 0;
+    }
+    if(isNaN(clothsInput) || clothsInput < 0) {
+        clothsError.style.display = 'block';
+
+        totalExpenses.innerText = 0;
+        totalBalance.innerText = 0;
+    }
+
+    /* error handle balance */
+    // if(totalExp > incomeInput) {
+    //     balanceError.style.display = 'block';
+    // }
+}
+
 
 document.getElementById('save-btn').addEventListener('click', function() {
     let saveInput = document.getElementById('save-input').value;
